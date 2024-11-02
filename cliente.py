6a8d6aa054be379ad0aa9626d90e1f9b75a2bfea
Erick Hernandez <janacua96@gmail.com>
2024-11-01 20:58:00 -0600

    Tarea_1

--- cliente.py
+++ cliente.py
@@ -8,5 +8,34 @@ def get_usuario():
     print(respuesta.json())
 
 
+def put_favorito():
+    respuesta = requests.put(URL_BASE + "/usuario/favorito",
+        params={"nombre": "Bistec",
+        "ultima_fecha": "21 de agosto de 2024",
+        "id": 1})
+    print(respuesta.json())
+
+def get_item():
+    respuesta = requests.get(URL_BASE + "/item", params={"id": "ajsdh"})
+    print(respuesta.json())
+
+
+def post_usuario():
+    respuesta = requests.post(URL_BASE + "/usuario",
+            json={"nombre": "Antonio",
+            "apellido": "Banderas",
+            "edad": 65,
+            "favoritos": []})
+    print(respuesta.json())
+
+
+def get_tarea1():
+    respuesta = requests.get(URL_BASE + "/tarea-1")
+    print(respuesta.json())
+
 if __name__ == "__main__":
-    get_usuario()
+    #get_usuario()
+    #put_favorito()
+    #get_item()
+    #post_usuario()
+    get_tarea1()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from super_db import usuarios, items
 
 app = FastAPI()
@@ -7,9 +8,9 @@ app = FastAPI()
 def get_base():
     return {"Mensaje": "Mundo"}
 
-# @app.get("/tarea-1")
-# def get_tarea1():
-#     return {"respuesta": "Primer tarea realizada"}
+@app.get("/tarea-1")
+def get_tarea1():
+    return {"respuesta": "Primer tarea realizada"}
 
 
 @app.get("/usuario/{id}")
@@ -19,3 +20,34 @@ def get_usuario(id: int):
         return "No se encontró el usuario"
     return usuario
 
+
+@app.put("/usuario/favorito")
+def put_favorito(nombre: str, ultima_fecha: str, id: int):
+    usuario = usuarios.get(id)
+    if not usuario:
+        return "No se encontró el usuario"
+    usuario['favoritos'].append({
+        "nombre": nombre, "ultima-fecha": ultima_fecha})
+    return usuario
+
+
+@app.get("/item")
+def get_usuario(id: str):
+    item = items.get(id)
+    if not item:
+        return "No se encontró el item"
+    return item
+
+
+class Usuario(BaseModel):
+    nombre: str
+    apellido: str
+    edad: int
+    favoritos: list
+
+@app.post("/usuario")
+def post_usuario(usuario: Usuario):
+    ultimo_id = len(usuarios)
+    usuarios[ultimo_id + 1] = usuario.model_dump()
+    print(usuarios)
+    return "Guardado"
